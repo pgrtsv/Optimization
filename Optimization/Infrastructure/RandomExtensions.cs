ba10861c17a2d947f8a3d936b8c1045434610292
7astafev18 <48848871+7astafev18@users.noreply.github.com>
2019-10-30 02:36:11 +0500

    В RandomExtensions добавлен метод GetRandomFrom для генерации перечисления из списка. Актуально, когда по списку уникальных сущностей необходимо сгенерировать другой список (который может содержать повторы), например, по списку уникальных товаров – генерируем заявку.

--- Optimization/Infrastructure/RandomExtensions.cs
+++ Optimization/Infrastructure/RandomExtensions.cs
@@ -27,5 +27,17 @@ namespace Optimization.Infrastructure
             if (maxValue <= minValue) throw new ArgumentException();
             return random.NextDouble() * (maxValue - minValue) + minValue;
         }
+
+        public static IEnumerable<T> GetRandomFrom<T>(this Random random, IList<T> list, int count)
+        {
+            if (count <= 0) throw new ArgumentException();
+            if (list?.Count <= 0) throw new ArgumentException();
+
+            for (int i = 0; i < count; i++)
+            {
+                var randomIndex = random.Next(0, list.Count - 1);
+                yield return list[randomIndex];
+            }
+        }
     }
 }
