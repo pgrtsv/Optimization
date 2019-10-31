9a8e21c135210b596a0515885f853dbed1b190b7
7astafev18 <48848871+7astafev18@users.noreply.github.com>
2019-10-31 23:03:28 +0500

    Изменение в генераторе товаров: - Теперь товары именуются: «Товар_1», «Товар_2» и т.д. - Генерируется больше дешёвых и маленьких товаров, чем дорогих и большой. Однако, большой и дешёвый товар или маленький и дорогой товар – не сгенериреуется.

--- Optimization.DataGeneration/GoodGenerator.cs
+++ Optimization.DataGeneration/GoodGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Optimization.DailyModel;
 using Optimization.Infrastructure;
 
@@ -12,37 +13,81 @@ namespace Optimization.DataGeneration
     {
         private static readonly Random _random = new Random();
 
-        #region Значения границ свойст товара при генерации по умолчанию
+        #region Константы для генерации цен.
 
-        public const int MinPrice = 20;
+        /* Все массивы в этом регионе должны иметь равное количество элементов.
+         * Подразумивается, что элементы следует от меньшего товара к большему (по размеру). */
 
-        public const int MaxPrice = 40000;
+        /// <summary>
+        /// Массив устанавливает вес на генерации товара определенного размера/цены.
+        /// </summary>
+        private static readonly int[] GoodGenerationWeights = { 80, 15, 5 };
 
-        public const double MinVolume = 0.00001;
+        /// <summary>
+        /// Массив устанавливает границы объема товаров.
+        /// </summary>
+        private static readonly (double, double)[] VolumeGenerationBorders = 
+            {(0.0001, 0.004), (0.004, 0.5), (0.5, 3)};
 
-        public const double MaxVolume = 10;
+        /// <summary>
+        /// Массив устанавливает границы цены товаров разного объема (большие по объему - стоят больше).
+        /// </summary>
+        private static readonly (int, int)[] PriceGenerationBorders = 
+            {(10, 500), (500, 10000), (7000, 100000)};
+        /* Заметка по границам товаров, я понимаю, что какое-нибудь маленькое кольцо может стоить
+         * больше громадного холодильника, но для простоты решил так: чем больше товар, тем больше цена. */
+
+        /* В целом, решение с массивами – грязное, возможно, что-то более
+         * чистое получилось бы с перечислением, но этот класс для разовой работы,
+         * так что пусть будет так. */
 
         #endregion
 
         /// <summary>
         /// Генерирует перечисление уникальных товаров.
         /// </summary>
+        /// <param name="count">Количество товаров, которое необходимо сгенерировать.</param>
+        /// <returns>Перечисление уникальных товаров.</returns>
         public static IEnumerable<Good> GenerateUniqueGoods(int count)
         {
-            /* Для простоты названия товаров – числовой идентификатор. */
-            /* Генерируем уникальные числовые индетификаторы. */
-            var digitNumbers = count.ToString().Length;
-            var minValue = (int) Math.Pow(10, digitNumbers);
-            var maxValue = (int) Math.Pow(10, digitNumbers + 1) - 1;
-            var uniqueNumbers = _random.GetDistinctRandomNumbers(count, minValue, maxValue);
-            
+            /*  */
             for (int i = 0; i < count; i++)
             {
-                var name = uniqueNumbers[i].ToString();
-                var price = _random.Next(MinPrice, MaxPrice);
-                var volume = _random.NextDouble(MinVolume, MaxVolume);
-                yield return new Good(volume, price, name);
+                /* Для простоты названия товаров – "товар_[числовой идентификатор]". */
+                var name = $"Товар_{i}";
+                var (volume, price) = GenerateVolumeAndPrice();
+                yield return new Good(volume, (decimal) price, name);
             }
+        }
+
+        /// <summary>
+        /// Генерирует объем товара и его цену с учётом весов генерации <see cref="GoodGenerationWeights"/>.
+        /// </summary>
+        /// <returns>Кортеж, где первый элемент – это объем товара в метрах кубических, второй – цена в рублях.</returns>
+        private static (double, double) GenerateVolumeAndPrice()
+        {
+            if(GoodGenerationWeights.Length != VolumeGenerationBorders.Length 
+               || GoodGenerationWeights.Length != PriceGenerationBorders.Length)
+                throw new Exception();
+            
+            /* Генерируем "класс" товара, по сути индекс в массивах выше. */
+            var goodClassIndex = 0;
+            var randomValue = _random.Next(0, GoodGenerationWeights.Sum());
+            var rightBorder = GoodGenerationWeights[0];
+            for (int i = 0; i < GoodGenerationWeights.Length - 1; i++)
+            {
+                goodClassIndex = i;
+                if(randomValue < rightBorder) break;
+                rightBorder += GoodGenerationWeights[i + 1];
+            }
+            
+            var (minVolume, maxVolume) = VolumeGenerationBorders[goodClassIndex];
+            var volume = Math.Round(_random.NextDouble(minVolume, maxVolume), 4);
+
+            var (minPrice, maxPrice) = PriceGenerationBorders[goodClassIndex];
+            var price = _random.Next(minPrice, maxPrice);
+
+            return (volume, price);
         }
     }
 }
