10068d59df153596b8f6b61c077f6b3e000c270e
Hatebearz <sprite96@ya.ru>
2019-12-26 15:49:33 +0500

    Сделан простой вариант реал-тайм сервиса. Нужно разобраться с потоками.

--- Optimization.UI/Controls/Map.cs
+++ Optimization.UI/Controls/Map.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Media;
+using DynamicData;
 using Optimization.Core;
 using Optimization.DailyModel;
 using ReactiveUI;
@@ -18,7 +22,15 @@ namespace Optimization.UI.Controls
                 x => x.CityMap,
                 (x, value) => x.CityMap = value);
 
+        public static readonly AvaloniaProperty<SimulationService> SimulationServiceProperty =
+            AvaloniaProperty.RegisterDirect<Map, SimulationService>(
+                nameof(SimulationService),
+                x => x.SimulationService,
+                (x, value) => x.SimulationService = value);
+
         private ICityMap _cityMap;
+
+        private SimulationService _simulationService;
 
         public Map()
         {
@@ -27,14 +39,35 @@ namespace Optimization.UI.Controls
                 Children.Clear();
                 if (CityMap == null) return;
                 foreach (var cityRoad in CityMap.Roads) Children.Add(CreateElementForRoad(cityRoad));
-                foreach (var cityPlace in CityMap.Places) Children.Add(CreateElementForPlace(cityPlace));
+                foreach (var cityPlace in CityMap.Places)
+                {
+                    Children.Add(CreateElementForPlace(cityPlace));
+                    Children.Add(LabelPlace(cityPlace));
+                }
                 RenderTransform = new TransformGroup
                 {
-                    Children = new Transforms {new TranslateTransform(500, 500), new ScaleTransform(0.5, 0.5)}
+                    Children = new Transforms { new TranslateTransform(500, 500), new ScaleTransform(0.5, 0.5) }
                 };
             });
             this.WhenAnyValue(x => x.CityMap)
+                .ObserveOn(RxApp.MainThreadScheduler)
                 .Subscribe(x => DrawCommand.Execute(null));
+            this.WhenAnyValue(x => x.SimulationService)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(x =>
+                {
+                    if (x == null) return;
+                    RecolorRoads();
+                    x.Interval
+                        .ObserveOn(RxApp.MainThreadScheduler)
+                        .Subscribe(_ => RecolorRoads());
+                });
+        }
+
+        public SimulationService SimulationService
+        {
+            get => _simulationService;
+            set => SetAndRaise(SimulationServiceProperty, ref _simulationService, value);
         }
 
         public ICityMap CityMap
@@ -45,8 +78,18 @@ namespace Optimization.UI.Controls
 
         public ICommand DrawCommand { get; }
 
+        public void RecolorRoads()
+        {
+            if (SimulationService == null) return;
+            foreach (var child in Children.OfType<Line>())
+            {
+                var usage = SimulationService.RoadsUsage.First(x => x.Road.Equals(child.DataContext)).Usage;
+                child.Stroke = GetBrushFromRoadUsage(usage);
+            }
+        }
 
-        public Control CreateElementForPlace(ICityPlace cityPlace)
+
+        public Ellipse CreateElementForPlace(ICityPlace cityPlace)
         {
             var ellipse = new Ellipse {Width = 10, Height = 10, ZIndex = 2};
             if (cityPlace is Warehouse)
@@ -60,29 +103,48 @@ namespace Optimization.UI.Controls
             return ellipse;
         }
 
-        public Control CreateElementForRoad(ICityRoad cityRoad)
+        public TextBlock LabelPlace(ICityPlace cityPlace)
+        {
+            var textBlock = new TextBlock();
+            textBlock.Background = new SolidColorBrush(Color.FromRgb(200, 200, 200), 0.3);
+            textBlock.ZIndex = 2;
+            SetLeft(textBlock, cityPlace.Coordinates.X);
+            SetTop(textBlock, cityPlace.Coordinates.Y);
+
+            switch (cityPlace)
+            {
+                case IWarehouse warehouse:
+                    textBlock.Text = "Склад";
+                    break;
+                case ISalePoint salePoint:
+                    textBlock.Text = "Точка продаж";
+                    break;
+            }
+
+            return textBlock;
+        }
+
+        private ISolidColorBrush GetBrushFromRoadUsage(RoadUsage roadUsage)
+        {
+            return roadUsage switch
+            {
+                RoadUsage.High => Brushes.Red,
+                RoadUsage.Medium => Brushes.Yellow,
+                RoadUsage.Low => Brushes.Green
+            };
+        }
+
+        public Line CreateElementForRoad(ICityRoad cityRoad)
         {
             var line = new Line
             {
                 StartPoint = new Point(cityRoad.FirstPlace.Coordinates.X, cityRoad.FirstPlace.Coordinates.Y),
                 EndPoint = new Point(cityRoad.SecondPlace.Coordinates.X, cityRoad.SecondPlace.Coordinates.Y),
                 ZIndex = 1,
-                StrokeThickness = 3
+                StrokeThickness = 3,
+                DataContext = cityRoad,
+                Stroke = Brushes.Black
             };
-            switch (cityRoad.Rank)
-            {
-                case RoadRank.High:
-                    line.Stroke = Brushes.Red;
-                    break;
-                case RoadRank.Medium:
-                    line.Stroke = Brushes.Yellow;
-                    break;
-                case RoadRank.Low:
-                    line.Stroke = Brushes.Green;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
 
             return line;
         }
--- Optimization.UI/ViewModels/MainWindowViewModel.cs
+++ Optimization.UI/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using DynamicData;
 using Optimization.Core;
 using Optimization.DailyModel;
 using Optimization.DataGeneration;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace Optimization.UI.ViewModels
 {
@@ -16,12 +21,32 @@ namespace Optimization.UI.ViewModels
 
         public ObservableCollection<Vehicle> Vehicles { get; }
 
+        public SimulationService SimulationService { get; }
+        public double[] AvailableSimulationTimes { get; }
+
         public MainWindowViewModel()
         {
+            AvailableSimulationTimes = new []{1.0, 10, 60, 100, 600};
+
             Goods = new Goods(GoodGenerator.GenerateUniqueGoods(50).Cast<IGood>().ToList());
             CityMap = new CityMapGenerator().Generate(Goods);
             VehicleModels = new ObservableCollection<VehicleModel>(new VehicleModelGenerator().GenerateUniqueVehicleModels(20));
             Vehicles = new ObservableCollection<Vehicle>(new VehicleGenerator().GenerateUniqueVehicles(50, VehicleModels));
+            SimulationService = new SimulationService(CityMap);
+            var canStartSimulation = SimulationService.WhenAnyValue(x => x.IsRunning)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Select(x => !x);
+            StartSimulationCommand = ReactiveCommand.Create(() =>
+            {
+                SimulationService.Start();
+            }, canStartSimulation);
+
+            var canStopSimulation = SimulationService.WhenAnyValue(x => x.IsRunning);
+            StopSimulationCommand = ReactiveCommand.Create(() => SimulationService.Stop(), canStopSimulation);
+            SimulationService.Start();
         }
+
+        public ReactiveCommand<Unit, Unit> StartSimulationCommand { get; }
+        public ReactiveCommand<Unit, Unit> StopSimulationCommand { get; }
     }
 }
