df1bc6a398da4e4c5d7a45e21c3e968e52fbc7f8
7astafev18 <48848871+7astafev18@users.noreply.github.com>
2020-01-07 15:25:25 +0500

    Изменения в генерации заказа: - более вариативная генерация: теперь при генерации может быть сгенерировано включения большего количества товарных позиций (экземпляров IGood); - для простоты – в заказе не запрашивается более 400 штук по одному товару; - немного изменены веса на генерируемые объемы заказа.

--- Optimization/DailyModel/SalePoint.cs
+++ Optimization/DailyModel/SalePoint.cs
@@ -20,7 +20,7 @@ namespace Optimization.DailyModel
         /// <summary>
         /// Массив весов для генерации заказа.
         /// </summary>
-        private static readonly int[] GoodGenerationWeights = { 2, 5, 30, 40, 15, 6, 2 };
+        private static readonly int[] GoodGenerationWeights = { 2, 5, 25, 40, 20, 6, 2 };
 
         /// <summary>
         /// Массив устанавливает границы объема заказа (в м^3).
@@ -52,24 +52,50 @@ namespace Optimization.DailyModel
             /* Чтобы не вызывать каждый раз LINQ для выборки уже взятых в заказ товаров. */
             var selectedGoods = new List<IGood>();
 
+            /* Генерируем границы объема в пределах которых будет сгенерирован заказ. */
             var weightIndex = _random.GetWeightIndexFrom(GoodGenerationWeights);
             var (minTotalVolume, maxTotalVolume) = VolumeGenerationRanges[weightIndex];
             var remainingMaxVolume = maxTotalVolume;
 
+            /* Пока не войдем в сгенерированные границы объема заказа, продолжаем генерировать заказ. */
             while (remainingMaxVolume > minTotalVolume && remainingMaxVolume <= maxTotalVolume)
             {
-                if (neededGoods.Count > 1 && remainingMaxVolume > minTotalVolume)
+                /* Если уже сгенерировано некоторое число товарных позиций, и уже находимся (но ещё не превышаем)
+                 * границы объема заказа, то с некоторой вероятностью можем закончить генерацию. */
+                if (neededGoods.Count > 10 && remainingMaxVolume > minTotalVolume)
                     if (!_random.NextBool(220/neededGoods.Count, (int) 0.9*neededGoods.Count))
                         return new Order(this, neededGoods);
+                
+                /* Выбираем случайный товар. */
                 var good = _random.PeekRandomFrom((IList<IGood>) _goods, selectedGoods);
-                var weights = GetGoodCountWeights(good);
-                var count = GenerateGoodCount(good, remainingMaxVolume, weights);
-                
-                /* Чтобы не было малого количества типов товаров в заказе. */
-                if (neededGoods.Count < 8 && _random.NextBool(90, 10))
-                    while (good.Volume * count > (maxTotalVolume - minTotalVolume) / 3)
-                        count /= 2;
-                
+                while(good.Volume > remainingMaxVolume)
+                    good = _random.PeekRandomFrom((IList<IGood>)_goods, selectedGoods);
+
+                /* Генерируем количество данного товара в заказе. */
+                var countGenerationWeights = GetGoodCountGenerationWeights(good);
+                var count = GenerateGoodCount(good, remainingMaxVolume, countGenerationWeights);
+
+                /* Небольшой костыль (так как генерация количества товара в заказе не идеальна):
+                 * если вдруг текущее количество товаров – 1/5 от всего заказа, с большей вероятностью
+                 * сокращаем это количество. */
+                if (good.Volume * count > maxTotalVolume / 5
+                    && count > 1
+                    && _random.NextBool(95, 5))
+                {
+                    /* Определяем порядок числа. */
+                    var countNumberOrder = 0;
+                    while (count / Math.Pow(10, countNumberOrder) > 10)
+                        countNumberOrder++;
+
+                    /* Сокращаем количество товаров в несколько раз (случайная величина). */
+                    var maxDivider = countNumberOrder == 0 ? 3 : (int)Math.Pow(10, countNumberOrder);
+                    /* Подстраховка, если вдруг в результате сокращения и приведения к int – получили ноль,
+                     * то сокращаем в два раза исходное количество. */
+                    var reducedCount = count / _random.Next(2, maxDivider);
+                    count = reducedCount > 0
+                        ? reducedCount : count / 2;
+                }
+
                 remainingMaxVolume -= good.Volume * count;
                 neededGoods.Add(good, count);
                 selectedGoods.Add(good);
@@ -78,19 +104,33 @@ namespace Optimization.DailyModel
             return new Order(this, neededGoods);
         }
 
+        /// <summary>
+        /// Генерирует количество товара в заказе.
+        /// </summary>
+        /// <param name="good">Товар.</param>
+        /// <param name="remainingVolume">Допустимый объем в заказе.</param>
+        /// <param name="weights">Массив весов на количество товара.</param>
+        /// <returns>Количество товара в заказе.</returns>
         private int GenerateGoodCount(IGood good, double remainingVolume, int[] weights)
         {
             var totalMaxCount = (int)(remainingVolume / good.Volume);
+
+            /* Генерируем небольше 400 товаров. */
+            if (totalMaxCount > 400)
+                totalMaxCount = _random.Next(200, 400);
+
             var (minCountBorder, maxCountBorder) = _random.NextIntRange(1, totalMaxCount, weights);
-            return _random.Next(minCountBorder, maxCountBorder);
+            var count = _random.Next(minCountBorder, maxCountBorder);
+
+            return count;
         }
 
-        public int[] GetGoodCountWeights(IGood good)
+        public int[] GetGoodCountGenerationWeights(IGood good)
         {
             /* Если товар относится к наименьшой по объемам группе -
              * больший вес на генерацию большего количество товаров,
              * если товар относится к большой по объемам группе -
-             * больший вес на генерацию меньшего количества товаров.*/
+             * больший вес на генерацию меньшего количества товаров. */
             var firstWeight = (int)(100 * good.Volume);
             var thirdWeight = (int)(2 / good.Volume);
             var secondWeight = (firstWeight + thirdWeight) / 4;
