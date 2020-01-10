0ab874916e341c581ee8b19b372b9a55c0d9d71f
7astafev18 <48848871+7astafev18@users.noreply.github.com>
2020-01-10 10:05:16 +0500

    Добавлена сборка для тестов. В ней добавлен тест для реализации алгоритма Дейкстры.

---
+++ Optimization.Tests/Infrastructure/DijkstraAlgorithm/DijkstraResolverTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Optimization.Core;
+using Optimization.DailyModel;
+using Optimization.Infrastructure.DijkstraAlgorithm;
+using Optimization.Tests.TestModel;
+using Xunit;
+
+namespace Optimization.Tests.Infrastructure.DijkstraAlgorithm
+{
+    public class DijkstrasResolverTests
+    {
+
+        [Fact]
+        public void FindShortWayTest()
+        {
+            var a = new CityPlace(new Coordinate(0, 0));
+            var b = new CityPlace(new Coordinate(1, 4));
+            var c = new CityPlace(new Coordinate(2, -1));
+            var d = new CityPlace(new Coordinate(5, -1));
+            var e = new CityPlace(new Coordinate(6, 4));
+
+            var ac = new SelfWeightedCityRoad(a, c, 2);
+            var ab = new SelfWeightedCityRoad(a, b, 5);
+            var cb = new SelfWeightedCityRoad(c, b, 2);
+            var cd = new SelfWeightedCityRoad(c, d, 6);
+            var be = new SelfWeightedCityRoad(b, e, 7);
+            var de = new SelfWeightedCityRoad(d, e, 1);
+
+            var cityPlaces = new HashSet<ICityPlace> {a, b, c, d, e};
+            var cityRoads = new HashSet<ICityRoad> {ac, ab, cb, cd, be, de};
+            var cityMap = new CityMap(cityPlaces, cityRoads);
+
+            var resolver = new DijkstraResolver();
+            var resolve = resolver.Resolve(cityMap, a);
+
+            var expected = new HashSet<ICityRoad> { ac, cd, de };
+            var actual = resolve.FindShortWayTo(e);
+
+            Assert.Equal(expected, actual);
+        }
+        
+    }
+}
---
+++ Optimization.Tests/TestModel/SelfWeightedCityRoad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Optimization.Core;
+
+namespace Optimization.Tests.TestModel
+{
+    public class SelfWeightedCityRoad: ICityRoad
+    {
+        public SelfWeightedCityRoad(ICityPlace firstPlace, ICityPlace secondPlace, double weight)
+        {
+            FirstPlace = firstPlace;
+            SecondPlace = secondPlace;
+            Weight = weight;
+        }
+
+        public ICityPlace FirstPlace { get; }
+        public ICityPlace SecondPlace { get; }
+        public double GetDistance() => FirstPlace.Coordinates.DistanceTo(SecondPlace.Coordinates);
+
+        public RoadRank Rank { get; }
+        public RoadUsage Usage { get; }
+        public double Weight { get; }
+        public void GenerateRoadUsage(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
