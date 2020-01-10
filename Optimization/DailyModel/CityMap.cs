9332adbe85cee6d45f91984376a2c44a4905cea9
7astafev18 <48848871+7astafev18@users.noreply.github.com>
2020-01-10 10:02:00 +0500

    В Optimization/Core/ICityMap.cs добавлены методы: для нахождения дорог, связанных с указанным местоположением; для нахождения ближайших связанных местоположений с указанным; для получения дороги связывающие местоположения. Все эти методы реализованы в Optimization/DailyModel/CityMap.cs.

--- Optimization/DailyModel/CityMap.cs
+++ Optimization/DailyModel/CityMap.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentValidation;
 using Optimization.Core;
 using Optimization.Validation;
@@ -16,5 +18,34 @@ namespace Optimization.DailyModel
 
         public ISet<ICityPlace> Places { get; }
         public ISet<ICityRoad> Roads { get; }
+
+        public IEnumerable<ICityRoad> GetRoadsFrom(ICityPlace cityPlace)
+        {
+            if(!Places.Contains(cityPlace))
+                throw new ArgumentException("Местоположение не принадлжеит городу.");
+
+            return Roads
+                .Where(x => x.FirstPlace == cityPlace || x.SecondPlace == cityPlace)
+                .Distinct();
+        }
+
+        public IEnumerable<ICityPlace> GetNeighborCityPlaces(ICityPlace cityPlace)
+        {
+            var roads = GetRoadsFrom(cityPlace).ToArray();
+
+            return roads.Select(x => x.SecondPlace)
+                .Union(roads.Select(x => x.FirstPlace))
+                .Distinct()
+                .Except(new[] {cityPlace});
+        }
+
+        public ICityRoad GetRoadBetween(ICityPlace firstCityPlace, ICityPlace secondCityPlace)
+        {
+            var road = Roads.FirstOrDefault(x =>
+                x.FirstPlace == firstCityPlace && x.SecondPlace == secondCityPlace ||
+                x.FirstPlace == secondCityPlace && x.SecondPlace == firstCityPlace);
+            
+            return road;
+        }
     }
 }
